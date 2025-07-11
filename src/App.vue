55a80e19f26e9b3edd9f38ced87fb26b388338b1
Miquéias <miqueiasbraga2001@gmail.com>
2025-07-11 20:43:12 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <h1 class="text-3xl font-bold underline text-sky-600">
+    Hello world!
+  </h1>
+  <button class="btn">Button</button>
+
+</template>
+
